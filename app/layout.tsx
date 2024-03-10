593333169d63ca455b8fc5c04b861217100c4e1a
gorkemzrnc <gorkemzurnaci@hotmail.com>
2024-03-10 15:40:07 +0300

    auth

---
+++ app/layout.tsx
@@ -0,0 +1,41 @@
+import { Nunito } from "next/font/google";
+import ClientOnly from "./components/ClientOnly";
+import RegisterModal from "./components/modals/RegisterModal";
+import Navbar from "./components/navbar/Navbar";
+import "./globals.css";
+import ToasterProvider from "./providers/ToasterProvider";
+import { Toaster } from 'react-hot-toast';
+import LoginModal from "./components/modals/LoginModal";
+import getCurrentUser from "./actions/getCurrentUser";
+
+
+export const metadata = {
+  title: "Airbnb",
+  description: "Airbnb clone",
+};
+
+const font = Nunito({ 
+  subsets: ['latin'], 
+});
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const currentUser = await getCurrentUser();
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ClientOnly>
+          <ToasterProvider/>
+          <RegisterModal />
+          <LoginModal />
+          <Navbar currentUser= {currentUser}/>
+        </ClientOnly>
+        
+        {children}
+      </body>
+    </html>
+  );
+}
